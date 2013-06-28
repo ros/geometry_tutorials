82964f604457c45339a5051a13d1e5439f7407a7
William Woodall <william@osrfoundation.org>
2013-06-28 14:14:25 -0700

    Constructing a Quaternion from rpy is deprecated

--- turtle_tf/src/turtle_tf_broadcaster.cpp
+++ turtle_tf/src/turtle_tf_broadcaster.cpp
@@ -10,7 +10,9 @@ void poseCallback(const turtlesim::PoseConstPtr& msg){
   static tf::TransformBroadcaster br;
   tf::Transform transform;
   transform.setOrigin( tf::Vector3(msg->x, msg->y, 0.0) );
-  transform.setRotation( tf::Quaternion(msg->theta, 0, 0) );
+  tf::Quaternion q;
+  q.setRPY(msg->theta, 0, 0);
+  transform.setRotation(q);
   br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", turtle_name));
 }
 
