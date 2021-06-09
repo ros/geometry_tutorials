0145301960e265a72f9a80bcb0058a3c364588cb
kurshakuz <abilkasov@gmail.com>
2021-06-09 15:23:58 +0600

    Migrate turtle_tf2 tutorial package to ROS2 (#34)
    * add dummy robot tf pkg
    
    * remove old packages
    
    * update package name and add launch file
    
    * turtle reaches target
    
    * add rviz config
    
    * use params for naming
    
    * remove old broadcaster
    
    * license and styling
    
    * update license date
    
    * rename package, update metapackage info
    
    * Update package.xml
    
    * update metapackage cmakelists
    
    * update maintainer
    
    * add example tf2 static broadcaster
    
    * add more comments to static broadcaster
    
    * add transforms3d to dependency
    
    * update node names
    
    * update dependency name
    
    * update maintainers in geometry_tutorials/package.xml
    
    Co-authored-by: Alejandro Hernández Cordero <ahcorde@gmail.com>
    
    * update return format in turtle_tf2_py/turtle_tf2_py/static_turtle_tf2_broadcaster.py
    
    Co-authored-by: Alejandro Hernández Cordero <ahcorde@gmail.com>
    
    * alphabetize deps in package.xml
    
    * fix typo in turtle_tf2_py/turtle_tf2_py/turtle_tf2_listener.py
    
    Co-authored-by: Audrow Nash <audrow@hey.com>
    
    * add tests and fix test errors
    
    * update copyright years in tests
    
    Co-authored-by: Alejandro Hernández Cordero <ahcorde@gmail.com>
    Co-authored-by: Audrow Nash <audrow@hey.com>

---
+++ turtle_tf2_py/setup.py
@@ -0,0 +1,37 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'turtle_tf2_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob(os.path.join('launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'rviz'),
+            glob(os.path.join('rviz', '*.rviz'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Shyngyskhan Abilkassov',
+    maintainer_email='abilkasov@gmail.com',
+    description=(
+        'turtle_tf2_py demonstrates how to write a ROS2 Python tf2 broadcaster and '
+        'listener with the turtlesim. The turtle_tf2_listener commands turtle2 to '
+        'follow turtle1 around as you drive turtle1 using the keyboard.'
+    ),
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'turtle_tf2_broadcaster = turtle_tf2_py.turtle_tf2_broadcaster:main',
+            'turtle_tf2_listener = turtle_tf2_py.turtle_tf2_listener:main',
+            'static_turtle_tf2_broadcaster = turtle_tf2_py.static_turtle_tf2_broadcaster:main',
+        ],
+    },
+)
---
+++ turtle_tf2_py/turtle_tf2_py/turtle_tf2_listener.py
@@ -0,0 +1,86 @@
+# Copyright 2021 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+from rclpy.node import Node
+import rclpy.time
+from tf2_ros import LookupException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+
+from turtlesim.srv import Spawn
+from geometry_msgs.msg import Twist
+import math
+from rclpy.duration import Duration
+
+
+class FrameListener(Node):
+    def __init__(self):
+        super().__init__('turtle_tf2_frame_listener')
+
+        self._tf_buffer = Buffer()
+        self._tf_listener = TransformListener(self._tf_buffer, self, spin_thread=False)
+
+        # Create a client to spawn a turtle
+        self.client = self.create_client(Spawn, 'spawn')
+
+        # Check if the service is available
+        while not self.client.wait_for_service(timeout_sec=5.0):
+            self.get_logger().info('service not available, waiting again...')
+
+        # Initialize request with turtle name and coordinates
+        request = Spawn.Request()
+        request.name = "turtle2"
+        request.x = float(4)
+        request.y = float(2)
+        request.theta = float(0)
+        # Call request
+        self.client.call_async(request)
+
+        # Create turtle2 velocity publisher
+        self.turtle_vel_ = self.create_publisher(Twist, 'turtle2/cmd_vel', 1)
+
+        # Call on_timer function every second
+        self._output_timer = self.create_timer(1.0, self.on_timer)
+
+    def on_timer(self):
+        from_frame_rel = 'turtle1'
+        to_frame_rel = 'turtle2'
+
+        # Look up for the transformation between turtle1 and turtle2 frames
+        # and send velocity commands for turtle2 to reach turtle1
+        try:
+            when = rclpy.time.Time()
+            trans = self._tf_buffer.lookup_transform(
+                to_frame_rel, from_frame_rel, when, timeout=Duration(seconds=1.0))
+
+            msg = Twist()
+            msg.angular.z = 1.0 * math.atan2(
+                trans.transform.translation.y, trans.transform.translation.x)
+            msg.linear.x = 0.5 * math.sqrt(
+                trans.transform.translation.x ** 2 + trans.transform.translation.y ** 2)
+            self.turtle_vel_.publish(msg)
+        except LookupException:
+            self.get_logger().info('transform not ready')
+
+
+def main():
+    rclpy.init()
+    node = FrameListener()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+
+    rclpy.shutdown()
