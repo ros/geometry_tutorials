e3d7a78e1a5e1a188b4b5d6d2fc2d80439877032
kurshakuz <abilkasov@gmail.com>
2021-07-10 19:47:21 +0600

    add inline comments on turtle translation and rotation

--- turtle_tf2_py/turtle_tf2_py/turtle_tf2_broadcaster.py
+++ turtle_tf2_py/turtle_tf2_py/turtle_tf2_broadcaster.py
@@ -53,9 +53,16 @@ class FramePublisher(Node):
         t.header.stamp = self.get_clock().now().to_msg()
         t.header.frame_id = 'world'
         t.child_frame_id = self.turtlename
+
+        # Turtle only exists in 2D, thus we get x and y translation
+        # coordinates from the message and set the z coordinate to 0
         t.transform.translation.x = msg.x
         t.transform.translation.y = msg.y
         t.transform.translation.z = 0.0
+
+        # For the same reason, turtle can only rotate around one axis 
+        # and this why we set rotation in x and y to 0 and obtain 
+        # rotation in z axis from the message
         q = tf_transformations.quaternion_from_euler(0, 0, msg.theta)
         t.transform.rotation.x = q[0]
         t.transform.rotation.y = q[1]
