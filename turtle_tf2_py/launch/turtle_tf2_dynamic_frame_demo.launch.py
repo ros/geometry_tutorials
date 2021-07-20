bff03094436a4195e9e874ac4b82cac75b489076
kurshakuz <abilkasov@gmail.com>
2021-07-20 16:54:47 +0600

    Add fixed and dynamic frame broadcaster nodes (#40)
    * add fixed and dynamic frame broadcaster nodes
    
    * add separate launch files for fixed and dynamic frame broadcasters
    
    * fix flake8 warning
    
    * Add target frame parameter to the listener node (#41) (#1)
    
    * add target frame parameter to the listener node
    
    * allow to modify parameter from command line
    
    * add launch argument to launch files
    
    * Remove empty line in turtle_tf2_py/launch/turtle_tf2_fixed_frame_demo.launch.py
    
    Co-authored-by: Alejandro Hernández Cordero <ahcorde@gmail.com>
    
    * Remove empty line in turtle_tf2_py/launch/turtle_tf2_dynamic_frame_demo.launch.py
    
    Co-authored-by: Alejandro Hernández Cordero <ahcorde@gmail.com>
    
    Co-authored-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

---
+++ turtle_tf2_py/launch/turtle_tf2_dynamic_frame_demo.launch.py
@@ -0,0 +1,41 @@
+# Copyright 2021 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    demo_nodes = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('turtle_tf2_py'), 'launch'),
+            '/turtle_tf2_demo.launch.py']),
+        launch_arguments={'target_frame': 'carrot1'}.items(),
+        )
+
+    return LaunchDescription([
+        demo_nodes,
+        Node(
+            package='turtle_tf2_py',
+            executable='dynamic_frame_tf2_broadcaster',
+            name='dynamic_broadcaster',
+        ),
+    ])
