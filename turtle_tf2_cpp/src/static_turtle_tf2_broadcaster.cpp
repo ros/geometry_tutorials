d956866f2c35ae71ef331613b56911ca1987e08c
kurshakuz <abilkasov@gmail.com>
2021-08-01 14:39:13 +0200

    fix test stderrors

--- turtle_tf2_cpp/src/static_turtle_tf2_broadcaster.cpp
+++ turtle_tf2_cpp/src/static_turtle_tf2_broadcaster.cpp
@@ -28,73 +28,75 @@
  */
 
 #include <geometry_msgs/msg/transform_stamped.hpp>
+
 #include <rclcpp/rclcpp.hpp>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/static_transform_broadcaster.h>
+
+#include <memory>
 
 using std::placeholders::_1;
 
 class StaticFramePublisher : public rclcpp::Node
 {
 public:
-    StaticFramePublisher(char *transformation[])
-        : Node("static_turtle_tf2_broadcaster")
-    {
-        tf_publisher_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
+  explicit StaticFramePublisher(char * transformation[])
+  : Node("static_turtle_tf2_broadcaster")
+  {
+    tf_publisher_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
 
-        // Publish static transforms once at startup
-        this->make_transforms(transformation);
-    }
+    // Publish static transforms once at startup
+    this->make_transforms(transformation);
+  }
 
 private:
-    void make_transforms(char *transformation[])
-    {
-        rclcpp::Time now;
-        geometry_msgs::msg::TransformStamped t;
+  void make_transforms(char * transformation[])
+  {
+    rclcpp::Time now;
+    geometry_msgs::msg::TransformStamped t;
 
-        t.header.stamp = now;
-        t.header.frame_id = "world";
-        t.child_frame_id = transformation[1];
+    t.header.stamp = now;
+    t.header.frame_id = "world";
+    t.child_frame_id = transformation[1];
 
-        t.transform.translation.x = atof(transformation[2]);
-        t.transform.translation.y = atof(transformation[3]);
-        t.transform.translation.z = atof(transformation[4]);
-        tf2::Quaternion q;
-        q.setRPY(
-            atof(transformation[5]),
-            atof(transformation[6]),
-            atof(transformation[7]));
-        t.transform.rotation.x = q.x();
-        t.transform.rotation.y = q.y();
-        t.transform.rotation.z = q.z();
-        t.transform.rotation.w = q.w();
+    t.transform.translation.x = atof(transformation[2]);
+    t.transform.translation.y = atof(transformation[3]);
+    t.transform.translation.z = atof(transformation[4]);
+    tf2::Quaternion q;
+    q.setRPY(
+      atof(transformation[5]),
+      atof(transformation[6]),
+      atof(transformation[7]));
+    t.transform.rotation.x = q.x();
+    t.transform.rotation.y = q.y();
+    t.transform.rotation.z = q.z();
+    t.transform.rotation.w = q.w();
 
-        tf_publisher_->sendTransform(t);
-    }
-    std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_publisher_;
+    tf_publisher_->sendTransform(t);
+  }
+  std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_publisher_;
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-    auto logger = rclcpp::get_logger("logger");
+  auto logger = rclcpp::get_logger("logger");
 
-    // Obtain parameters from command line arguments
-    if (argc != 8)
-    {
-        RCLCPP_INFO(logger, "Invalid number of parameters\nusage: "
-                    "ros2 run learning_tf2_cpp static_turtle_tf2_broadcaster "
-                    "child_frame_name x y z roll pitch yaw");
-        return 1;
-    }
-    if (strcmp(argv[1], "world") == 0)
-    {
-        RCLCPP_INFO(logger, "Your static turtle name cannot be 'world'");
-        return 1;
-    }
+  // Obtain parameters from command line arguments
+  if (argc != 8) {
+    RCLCPP_INFO(
+      logger, "Invalid number of parameters\nusage: "
+      "ros2 run learning_tf2_cpp static_turtle_tf2_broadcaster "
+      "child_frame_name x y z roll pitch yaw");
+    return 1;
+  }
+  if (strcmp(argv[1], "world") == 0) {
+    RCLCPP_INFO(logger, "Your static turtle name cannot be 'world'");
+    return 1;
+  }
 
-    // Pass parameters and initialize node
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<StaticFramePublisher>(argv));
-    rclcpp::shutdown();
-    return 0;
+  // Pass parameters and initialize node
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<StaticFramePublisher>(argv));
+  rclcpp::shutdown();
+  return 0;
 }
--- turtle_tf2_cpp/src/turtle_tf2_listener.cpp
+++ turtle_tf2_cpp/src/turtle_tf2_listener.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Open Source Robotics Foundation
+ * Copyright (c) 2021, Open Source Robotics Foundation, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,18 +27,18 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <chrono>
-
 #include <geometry_msgs/msg/transform_stamped.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 
 #include <rclcpp/rclcpp.hpp>
-
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/buffer.h>
 #include <tf2/exceptions.h>
-
 # include <turtlesim/srv/spawn.hpp>
+
+#include <chrono>
+#include <memory>
+#include <string>
 
 using std::placeholders::_1;
 using namespace std::chrono_literals;
@@ -46,93 +46,98 @@ using namespace std::chrono_literals;
 class FrameListener : public rclcpp::Node
 {
 public:
-    FrameListener()
-        : Node("turtle_tf2_frame_listener")
-    {
-        // Declare and acquire `target_frame` parameter
-        this->declare_parameter<std::string>("target_frame", "turtle1");
-        this->get_parameter("target_frame", target_frame_);
+  FrameListener()
+  : Node("turtle_tf2_frame_listener")
+  {
+    // Declare and acquire `target_frame` parameter
+    this->declare_parameter<std::string>("target_frame", "turtle1");
+    this->get_parameter("target_frame", target_frame_);
 
-        tf_buffer_ =
-            std::make_unique<tf2_ros::Buffer>(this->get_clock());
-        transform_listener_ =
-            std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+    tf_buffer_ =
+      std::make_unique<tf2_ros::Buffer>(this->get_clock());
+    transform_listener_ =
+      std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
-        // Create a client to spawn a turtle
-        rclcpp::Client<turtlesim::srv::Spawn>::SharedPtr spawner =
-            this->create_client<turtlesim::srv::Spawn>("spawn");
+    // Create a client to spawn a turtle
+    rclcpp::Client<turtlesim::srv::Spawn>::SharedPtr spawner =
+      this->create_client<turtlesim::srv::Spawn>("spawn");
 
-        // Check if the service is available
-        while (!spawner->wait_for_service(1s)) {
-            if (!rclcpp::ok()) {
-                RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
-                continue;
-            }
-            RCLCPP_INFO(this->get_logger(), "Service not available, waiting again...");
-        }
-
-        // Initialize request with turtle name and coordinates
-        // Note that x, y and theta are defined as floats in turtlesim/srv/Spawn
-        auto request = std::make_shared<turtlesim::srv::Spawn::Request>();
-        request->x = 4.0;
-        request->y = 2.0;
-        request->theta = 0.0;
-        request->name = "turtle2";
-        // Call request
-        auto result = spawner->async_send_request(request);
-
-        // Create turtle2 velocity publisher
-        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle2/cmd_vel", 1);
-
-        // Call on_timer function every second
-        timer_ = this->create_wall_timer(
-            1s, std::bind(&FrameListener::on_timer, this));
-
+    // Check if the service is available
+    while (!spawner->wait_for_service(1s)) {
+      if (!rclcpp::ok()) {
+        RCLCPP_ERROR(
+          this->get_logger(),
+          "Interrupted while waiting for the service. Exiting."
+        );
+        continue;
+      }
+      RCLCPP_INFO(
+        this->get_logger(),
+        "Service not available, waiting again..."
+      );
     }
+
+    // Initialize request with turtle name and coordinates
+    // Note that x, y and theta are defined as floats in turtlesim/srv/Spawn
+    auto request = std::make_shared<turtlesim::srv::Spawn::Request>();
+    request->x = 4.0;
+    request->y = 2.0;
+    request->theta = 0.0;
+    request->name = "turtle2";
+    // Call request
+    auto result = spawner->async_send_request(request);
+
+    // Create turtle2 velocity publisher
+    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle2/cmd_vel", 1);
+
+    // Call on_timer function every second
+    timer_ = this->create_wall_timer(
+      1s, std::bind(&FrameListener::on_timer, this));
+  }
 
 private:
-    void on_timer()
-    {
-        // Store frame names in variables that will be used to
-        // compute transformations
-        std::string from_frame_rel = target_frame_.c_str();
-        std::string to_frame_rel = "turtle2";
+  void on_timer()
+  {
+    // Store frame names in variables that will be used to
+    // compute transformations
+    std::string from_frame_rel = target_frame_.c_str();
+    std::string to_frame_rel = "turtle2";
 
-        geometry_msgs::msg::TransformStamped transformStamped;
+    geometry_msgs::msg::TransformStamped transformStamped;
 
-        // Look up for the transformation between target_frame and turtle2 frames
-        // and send velocity commands for turtle2 to reach target_frame
-        try
-        {
-            transformStamped = tf_buffer_->lookupTransform(to_frame_rel, from_frame_rel,
-                                                        tf2::TimePoint(),
-                                                        500ms);
-        }
-        catch (tf2::LookupException &ex)
-        {
-            RCLCPP_INFO(this->get_logger(), "transform not ready");
-            return;
-        }
-
-        geometry_msgs::msg::Twist vel_msg;
-        vel_msg.angular.z = 1.0 * atan2(transformStamped.transform.translation.y,
-                                        transformStamped.transform.translation.x);
-        vel_msg.linear.x = 0.5 * sqrt(pow(transformStamped.transform.translation.x, 2) +
-                                    pow(transformStamped.transform.translation.y, 2));
-        publisher_->publish(vel_msg);
+    // Look up for the transformation between target_frame and turtle2 frames
+    // and send velocity commands for turtle2 to reach target_frame
+    try {
+      transformStamped = tf_buffer_->lookupTransform(
+        to_frame_rel, from_frame_rel,
+        tf2::TimePoint(),
+        500ms);
+    } catch (tf2::LookupException & ex) {
+      RCLCPP_INFO(this->get_logger(), "transform not ready");
+      return;
     }
-    rclcpp::TimerBase::SharedPtr timer_;
-    std::shared_ptr<tf2_ros::TransformListener> transform_listener_;
-    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
-    std::string target_frame_;
+
+    geometry_msgs::msg::Twist vel_msg;
+    vel_msg.angular.z = 1.0 * atan2(
+      transformStamped.transform.translation.y,
+      transformStamped.transform.translation.x);
+    vel_msg.linear.x = 0.5 * sqrt(
+      pow(transformStamped.transform.translation.x, 2) +
+      pow(transformStamped.transform.translation.y, 2));
+    publisher_->publish(vel_msg);
+  }
+  rclcpp::TimerBase::SharedPtr timer_;
+  std::shared_ptr<tf2_ros::TransformListener> transform_listener_;
+  std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+  std::string target_frame_;
 };
 
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<FrameListener>());
-    rclcpp::shutdown();
-    return 0;
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<FrameListener>());
+  rclcpp::shutdown();
+  return 0;
 }
