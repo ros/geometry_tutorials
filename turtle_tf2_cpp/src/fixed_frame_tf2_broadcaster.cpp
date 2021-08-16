fee0baad7f5ffb42dbff712cf8e240f81e48f6b8
kurshakuz <abilkasov@gmail.com>
2021-08-16 14:47:30 +0200

    create fixed and dynamic frame broacaster nodes and laynch files C++

---
+++ turtle_tf2_cpp/src/fixed_frame_tf2_broadcaster.cpp
@@ -0,0 +1,64 @@
+// Copyright 2021 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <geometry_msgs/msg/transform_stamped.hpp>
+
+#include <rclcpp/rclcpp.hpp>
+#include <tf2_ros/transform_broadcaster.h>
+
+#include <memory>
+
+using namespace std::chrono_literals;
+
+class FixedFrameBroadcaster : public rclcpp::Node
+{
+public:
+  FixedFrameBroadcaster()
+  : Node("fixed_frame_tf2_broadcaster")
+  {
+    tf_publisher_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
+    timer_ = this->create_wall_timer(
+      100ms, std::bind(&FixedFrameBroadcaster::broadcast_timer_callback, this));
+  }
+
+private:
+  void broadcast_timer_callback()
+  {
+    rclcpp::Time now;
+    geometry_msgs::msg::TransformStamped t;
+
+    t.header.stamp = now;
+    t.header.frame_id = "turtle1";
+    t.child_frame_id = "carrot1";
+    t.transform.translation.x = 0.0;
+    t.transform.translation.y = 2.0;
+    t.transform.translation.z = 0.0;
+    t.transform.rotation.x = 0.0;
+    t.transform.rotation.y = 0.0;
+    t.transform.rotation.z = 0.0;
+    t.transform.rotation.w = 1.0;
+
+    tf_publisher_->sendTransform(t);
+  }
+  rclcpp::TimerBase::SharedPtr timer_;
+  std::shared_ptr<tf2_ros::TransformBroadcaster> tf_publisher_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<FixedFrameBroadcaster>());
+  rclcpp::shutdown();
+  return 0;
+}
