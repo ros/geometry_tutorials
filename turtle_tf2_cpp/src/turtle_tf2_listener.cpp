60f42808023b52f09ab95d6b3f84632c32ca043b
kurshakuz <abilkasov@gmail.com>
2021-08-17 18:04:51 +0200

    fix linters

--- turtle_tf2_cpp/src/turtle_tf2_listener.cpp
+++ turtle_tf2_cpp/src/turtle_tf2_listener.cpp
@@ -99,7 +99,8 @@ private:
 
     if (!transform_available_) {
       const auto timeout_ms_ = 100ms;
-      while(!tf_buffer_->canTransform(toFrameRel, fromFrameRel, tf2::TimePointZero, 0ms) && rclcpp::ok()) {
+      while(!tf_buffer_->canTransform(toFrameRel, fromFrameRel, tf2::TimePointZero, 0ms) 
+            && rclcpp::ok()) {
         RCLCPP_INFO(
           get_logger(), "waiting %ld ms for %s->%s transform to become available",
           timeout_ms_.count(), toFrameRel.c_str(), fromFrameRel.c_str());
@@ -107,8 +108,8 @@ private:
       }
       RCLCPP_INFO(get_logger(), "transform available");
       transform_available_ = true;
-    } 
-    
+    }
+
     if (transform_available_) {
       geometry_msgs::msg::TransformStamped transformStamped;
 
