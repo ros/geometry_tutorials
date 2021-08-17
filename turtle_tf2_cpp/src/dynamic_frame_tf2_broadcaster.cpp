6a5fa0e130be8eec55338c00b4735aebab0e5e9c
kurshakuz <abilkasov@gmail.com>
2021-08-17 11:02:20 +0200

    Fixed and dynamic frame broacaster nodes and launch files C++ (#52)
    * create fixed and dynamic frame broacaster nodes and laynch files C++
    
    * make PI constant

---
+++ turtle_tf2_cpp/src/dynamic_frame_tf2_broadcaster.cpp
@@ -0,0 +1,69 @@
+// Copyright 2021 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <geometry_msgs/msg/transform_stamped.hpp>
+
+#include <rclcpp/rclcpp.hpp>
+#include <tf2_ros/transform_broadcaster.h>
+
+#include <memory>
+
+using namespace std::chrono_literals;
+
+const double PI = 3.141592653589793238463;
+
+class DynamicFrameBroadcaster : public rclcpp::Node
+{
+public:
+  DynamicFrameBroadcaster()
+  : Node("dynamic_frame_tf2_broadcaster")
+  {
+    tf_publisher_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
+    timer_ = this->create_wall_timer(
+      100ms, std::bind(&DynamicFrameBroadcaster::broadcast_timer_callback, this));
+  }
+
+private:
+  void broadcast_timer_callback()
+  {
+    geometry_msgs::msg::TransformStamped t;
+
+    rclcpp::Time now = this->now();
+    double x = now.seconds() * PI;
+
+    rclcpp::Time stamp;
+    t.header.stamp = stamp;
+    t.header.frame_id = "turtle1";
+    t.child_frame_id = "carrot1";
+    t.transform.translation.x = 10 * sin(x);
+    t.transform.translation.y = 10 * cos(x);
+    t.transform.translation.z = 0.0;
+    t.transform.rotation.x = 0.0;
+    t.transform.rotation.y = 0.0;
+    t.transform.rotation.z = 0.0;
+    t.transform.rotation.w = 1.0;
+
+    tf_publisher_->sendTransform(t);
+  }
+  rclcpp::TimerBase::SharedPtr timer_;
+  std::shared_ptr<tf2_ros::TransformBroadcaster> tf_publisher_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<DynamicFrameBroadcaster>());
+  rclcpp::shutdown();
+  return 0;
+}
