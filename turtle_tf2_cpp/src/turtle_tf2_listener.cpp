185ec631b0925a9a29af6ea4f467ef147114afcb
kurshakuz <abilkasov@gmail.com>
2021-08-27 10:08:19 +0200

    update listener node (#53)
    * update listener node
    
    * fix the if statement
    
    * fix linters
    
    * fix linters
    
    * update suggestions
    
    * handle timer stopping
    
    * check if service is ready; check the spawn result; update canTransform code
    
    * fix linters
    
    * add more explanations to the boolean values
    
    * add more messages to the user
    
    * add space in exception handling
    
    * remove canTransform() call to simplify code
    
    * remove empty line
    
    * remove timeout in lookupTransform() call
    
    * add turtle_spawned falg check
    
    * add lambda function check of service call
    
    * change spawn is called variable name
    
    * eliminate else if statement handling
    
    * remove empty line
    
    * restructure if else structure in main timer function

--- turtle_tf2_cpp/src/turtle_tf2_listener.cpp
+++ turtle_tf2_cpp/src/turtle_tf2_listener.cpp
@@ -32,7 +32,9 @@ class FrameListener : public rclcpp::Node
 {
 public:
   FrameListener()
-  : Node("turtle_tf2_frame_listener")
+  : Node("turtle_tf2_frame_listener"),
+    turtle_spawning_service_ready_(false),
+    turtle_spawned_(false)
   {
     // Declare and acquire `target_frame` parameter
     this->declare_parameter<std::string>("target_frame", "turtle1");
@@ -44,36 +46,12 @@ public:
       std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
     // Create a client to spawn a turtle
-    rclcpp::Client<turtlesim::srv::Spawn>::SharedPtr spawner =
+    spawner_ =
       this->create_client<turtlesim::srv::Spawn>("spawn");
 
-    // Check if the service is available
-    while (!spawner->wait_for_service(1s)) {
-      if (!rclcpp::ok()) {
-        RCLCPP_ERROR(
-          this->get_logger(),
-          "Interrupted while waiting for the service. Exiting."
-        );
-        continue;
-      }
-      RCLCPP_INFO(
-        this->get_logger(),
-        "Service not available, waiting again..."
-      );
-    }
-
-    // Initialize request with turtle name and coordinates
-    // Note that x, y and theta are defined as floats in turtlesim/srv/Spawn
-    auto request = std::make_shared<turtlesim::srv::Spawn::Request>();
-    request->x = 4.0;
-    request->y = 2.0;
-    request->theta = 0.0;
-    request->name = "turtle2";
-    // Call request
-    auto result = spawner->async_send_request(request);
-
     // Create turtle2 velocity publisher
-    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle2/cmd_vel", 1);
+    publisher_ =
+      this->create_publisher<geometry_msgs::msg::Twist>("turtle2/cmd_vel", 1);
 
     // Call on_timer function every second
     timer_ = this->create_wall_timer(
@@ -88,41 +66,81 @@ private:
     std::string fromFrameRel = target_frame_.c_str();
     std::string toFrameRel = "turtle2";
 
-    geometry_msgs::msg::TransformStamped transformStamped;
+    if (turtle_spawning_service_ready_) {
+      if (turtle_spawned_) {
+        geometry_msgs::msg::TransformStamped transformStamped;
 
-    // Look up for the transformation between target_frame and turtle2 frames
-    // and send velocity commands for turtle2 to reach target_frame
-    try {
-      transformStamped = tf_buffer_->lookupTransform(
-        toFrameRel, fromFrameRel,
-        tf2::TimePoint(),
-        500ms);
-    } catch (tf2::LookupException & ex) {
-      RCLCPP_INFO(this->get_logger(), "transform not ready");
-      return;
+        // Look up for the transformation between target_frame and turtle2 frames
+        // and send velocity commands for turtle2 to reach target_frame
+        try {
+          transformStamped = tf_buffer_->lookupTransform(
+            toFrameRel, fromFrameRel,
+            tf2::TimePointZero);
+        } catch (tf2::TransformException & ex) {
+          RCLCPP_INFO(
+            this->get_logger(), "Could not transform %s to %s: %s",
+            toFrameRel.c_str(), fromFrameRel.c_str(), ex.what());
+          return;
+        }
+
+        geometry_msgs::msg::Twist msg;
+
+        static const double scaleRotationRate = 1.0;
+        msg.angular.z = scaleRotationRate * atan2(
+          transformStamped.transform.translation.y,
+          transformStamped.transform.translation.x);
+
+        static const double scaleForwardSpeed = 0.5;
+        msg.linear.x = scaleForwardSpeed * sqrt(
+          pow(transformStamped.transform.translation.x, 2) +
+          pow(transformStamped.transform.translation.y, 2));
+
+        publisher_->publish(msg);
+      } else {
+        RCLCPP_INFO(this->get_logger(), "Successfully spawned %s", result_.get()->name.c_str());
+        turtle_spawned_ = true;
+      }
+    } else {
+      // Check if the service is ready
+      if (spawner_->service_is_ready()) {
+        // Initialize request with turtle name and coordinates
+        // Note that x, y and theta are defined as floats in turtlesim/srv/Spawn
+        auto request = std::make_shared<turtlesim::srv::Spawn::Request>();
+        request->x = 4.0;
+        request->y = 2.0;
+        request->theta = 0.0;
+        request->name = "turtle2";
+
+        // Call request
+        using ServiceResponseFuture =
+          rclcpp::Client<turtlesim::srv::Spawn>::SharedFuture;
+        auto response_received_callback = [this](ServiceResponseFuture future) {
+            auto result = future.get();
+            if (strcmp(result->name.c_str(), "turtle2") == 0) {
+              turtle_spawning_service_ready_ = true;
+            } else {
+              RCLCPP_ERROR(this->get_logger(), "Service callback result mismatch");
+            }
+          };
+        result_ = spawner_->async_send_request(request, response_received_callback);
+      } else {
+        RCLCPP_INFO(this->get_logger(), "Service is not ready");
+      }
     }
-
-    geometry_msgs::msg::Twist msg;
-
-    static const double scaleRotationRate = 1.0;
-    msg.angular.z = scaleRotationRate * atan2(
-      transformStamped.transform.translation.y,
-      transformStamped.transform.translation.x);
-
-    static const double scaleForwardSpeed = 0.5;
-    msg.linear.x = scaleForwardSpeed * sqrt(
-      pow(transformStamped.transform.translation.x, 2) +
-      pow(transformStamped.transform.translation.y, 2));
-
-    publisher_->publish(msg);
   }
-  rclcpp::TimerBase::SharedPtr timer_;
-  std::shared_ptr<tf2_ros::TransformListener> transform_listener_;
+  // Boolean values to store the information
+  // if the service for spawning turtle is available
+  bool turtle_spawning_service_ready_;
+  // if the turtle was successfully spawned
+  bool turtle_spawned_;
+  rclcpp::Client<turtlesim::srv::Spawn>::SharedFuture result_;
+  rclcpp::Client<turtlesim::srv::Spawn>::SharedPtr spawner_{nullptr};
+  rclcpp::TimerBase::SharedPtr timer_{nullptr};
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_{nullptr};
+  std::shared_ptr<tf2_ros::TransformListener> transform_listener_{nullptr};
   std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
   std::string target_frame_;
 };
-
 
 int main(int argc, char * argv[])
 {
