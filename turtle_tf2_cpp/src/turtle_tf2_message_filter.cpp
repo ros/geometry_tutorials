c34ea84a682a79e73cc9690b510ce39e49a24725
kenny_wang <kennywangaigraphx@gmail.com>
2021-09-15 23:26:49 -0700

    Update files according to linting

--- turtle_tf2_cpp/src/turtle_tf2_message_filter.cpp
+++ turtle_tf2_cpp/src/turtle_tf2_message_filter.cpp
@@ -1,76 +1,92 @@
-   #include <rclcpp/rclcpp.hpp>
-   #include <geometry_msgs/msg/point_stamped.hpp>
-   
-   #include <tf2_ros/transform_listener.h>
-   #include <tf2_ros/message_filter.h>
-   #include <tf2_ros/buffer.h>
-   #include <tf2_ros/create_timer_ros.h>
-   #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-   #include <message_filters/subscriber.h>
-   
-   #include <chrono>
-   #include <memory>
-   #include <string>
-   
-   using std::placeholders::_1;
-   using namespace std::chrono_literals;
-   
-   
-   class PoseDrawer : public rclcpp::Node
-   {
-   public:
-     PoseDrawer()
-     : Node("turtle_tf2_pose_drawer")
-     {
-       auto node = rclcpp::Node::make_shared("tf2_ros_message_filter");
-       auto create_timer_interface = std::make_shared<tf2_ros::CreateTimerROS>(
-         node->get_node_base_interface(),
-         node->get_node_timers_interface());  
-       typedef std::chrono::duration<int> seconds_type;
-       seconds_type buffer_timeout(1);
-       
-       // Declare and acquire `target_frame` parameter
-       this->declare_parameter<std::string>("target_frame", "turtle1");
-       this->get_parameter("target_frame", target_frame_);
+// Copyright 2021 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-       rclcpp::Clock::SharedPtr clock = std::make_shared<rclcpp::Clock>(RCL_SYSTEM_TIME);
-       tf2_buffer_ = std::make_shared<tf2_ros::Buffer>(node->get_clock());
-       tf2_buffer_->setCreateTimerInterface(create_timer_interface);
-       tf2_listener_ =
-         std::make_shared<tf2_ros::TransformListener>(*tf2_buffer_);
-       point_sub_.subscribe(this, "/turtle3/turtle_point_stamped");
-       tf2_filter_ = std::make_shared<tf2_ros::MessageFilter<geometry_msgs::msg::PointStamped>>(point_sub_, *tf2_buffer_, target_frame_, 10, node, buffer_timeout); 
-       tf2_filter_->registerCallback(&PoseDrawer::msgCallback, this);
-     }  
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/point_stamped.hpp>
 
-   //  Callback to register with tf2_ros::MessageFilter to be called when transforms are available
-   private:
-     void msgCallback(const geometry_msgs::msg::PointStamped::SharedPtr point_ptr)
-     {
-       geometry_msgs::msg::PointStamped point_out;
-       try
-       {
-         tf2_buffer_->transform(*point_ptr, point_out, target_frame_);
-         RCLCPP_INFO(this->get_logger(), "point of turtle 3 in frame of turtle 1 Position(x:%f y:%f z:%f)\n", 
-             point_out.point.x,
-             point_out.point.y,
-             point_out.point.z);
-       }catch(tf2::TransformException &ex)
-       {
-         RCLCPP_WARN(this->get_logger(), "Failure %s\n", ex.what()); //Print exception which was caught
-       }    
-     }
-     std::string target_frame_;
-     std::shared_ptr<tf2_ros::Buffer> tf2_buffer_;
-     std::shared_ptr<tf2_ros::TransformListener> tf2_listener_;  
-     message_filters::Subscriber<geometry_msgs::msg::PointStamped> point_sub_;
-     std::shared_ptr<tf2_ros::MessageFilter<geometry_msgs::msg::PointStamped>> tf2_filter_;
-   };
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/message_filter.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/create_timer_ros.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <message_filters/subscriber.h>
 
-   int main(int argc, char * argv[])
-   {
-     rclcpp::init(argc, argv);
-     rclcpp::spin(std::make_shared<PoseDrawer>());
-     rclcpp::shutdown();
-     return 0;
-   }
+#include <chrono>
+#include <memory>
+#include <string>
+
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
+
+class PoseDrawer : public rclcpp::Node
+{
+public:
+  PoseDrawer()
+  : Node("turtle_tf2_pose_drawer")
+  {
+    auto node = rclcpp::Node::make_shared("tf2_ros_message_filter");
+    auto create_timer_interface = std::make_shared<tf2_ros::CreateTimerROS>(
+      node->get_node_base_interface(),
+      node->get_node_timers_interface());
+    typedef std::chrono::duration<int> seconds_type;
+    seconds_type buffer_timeout(1);
+
+    // Declare and acquire `target_frame` parameter
+    this->declare_parameter<std::string>("target_frame", "turtle1");
+    this->get_parameter("target_frame", target_frame_);
+
+    rclcpp::Clock::SharedPtr clock = std::make_shared<rclcpp::Clock>(RCL_SYSTEM_TIME);
+    tf2_buffer_ = std::make_shared<tf2_ros::Buffer>(node->get_clock());
+    tf2_buffer_->setCreateTimerInterface(create_timer_interface);
+    tf2_listener_ =
+      std::make_shared<tf2_ros::TransformListener>(*tf2_buffer_);
+    point_sub_.subscribe(this, "/turtle3/turtle_point_stamped");
+    tf2_filter_ = std::make_shared<tf2_ros::MessageFilter<geometry_msgs::msg::PointStamped>>(
+      point_sub_, *tf2_buffer_, target_frame_, 10, node, buffer_timeout);
+    tf2_filter_->registerCallback(&PoseDrawer::msgCallback, this);
+  }
+
+// Callback to register with tf2_ros::MessageFilter to be called when transforms are available
+
+private:
+  void msgCallback(const geometry_msgs::msg::PointStamped::SharedPtr point_ptr)
+  {
+    geometry_msgs::msg::PointStamped point_out;
+    try {
+      tf2_buffer_->transform(*point_ptr, point_out, target_frame_);
+      RCLCPP_INFO(
+        this->get_logger(), "point of turtle 3 in frame of turtle 1 Position(x:%f y:%f z:%f)\n",
+        point_out.point.x,
+        point_out.point.y,
+        point_out.point.z);
+    } catch (tf2::TransformException & ex) {
+      RCLCPP_WARN(
+        this->get_logger(), "Failure %s\n", ex.what());    // Print exception which was caught
+    }
+  }
+  std::string target_frame_;
+  std::shared_ptr<tf2_ros::Buffer> tf2_buffer_;
+  std::shared_ptr<tf2_ros::TransformListener> tf2_listener_;
+  message_filters::Subscriber<geometry_msgs::msg::PointStamped> point_sub_;
+  std::shared_ptr<tf2_ros::MessageFilter<geometry_msgs::msg::PointStamped>> tf2_filter_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<PoseDrawer>());
+  rclcpp::shutdown();
+  return 0;
+}
--- turtle_tf2_py/turtle_tf2_py/turtle_tf2_message_broadcaster.py
+++ turtle_tf2_py/turtle_tf2_py/turtle_tf2_message_broadcaster.py
@@ -1,53 +1,66 @@
-   import rclpy
-   from rclpy.node import Node
-   from rclpy.duration import Duration
+# Copyright 2021 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-   from geometry_msgs.msg import PointStamped, Point
-   from geometry_msgs.msg import Twist
-   from turtlesim.msg import Pose
-   from turtlesim.srv import Spawn
-   
-   
-   class PointPublisher(Node):
+from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import Twist
 
-       def __init__(self):
-           super().__init__('turtle_tf2_msg_broadcaster')
-        
-           self.client = self.create_client(Spawn, 'spawn')
-           while not self.client.wait_for_service(timeout_sec=1.0):
-               self.get_logger().info('service not available, waiting again...')
-           request = Spawn.Request()
-           request.name = 'turtle3'
-           request.x = float(4)
-           request.y = float(2)
-           request.theta = float(0)
-           self.client.call_async(request)
-        
-           self.vel_pub = self.create_publisher(Twist, "/turtle3/cmd_vel",1) 
-           self.sub = self.create_subscription(Pose, "/turtle3/pose",self.handle_turtle_pose, 1) 
-           self.sub
-           self.pub = self.create_publisher(PointStamped, "/turtle3/turtle_point_stamped", 1)
-        
-       def handle_turtle_pose(self,msg):
-           vel_msg = Twist()
-           vel_msg.linear.x =1.0
-           vel_msg.angular.z = 1.0
-           self.vel_pub.publish(vel_msg) 
-           
-           ps = PointStamped()
-           ps.header.stamp = self.get_clock().now().to_msg()
-           ps.header.frame_id = 'world'
-           ps.point.x = msg.x
-           ps.point.y = msg.y
-           ps.point.z = 0.0
-           self.pub.publish(ps)
-    
- 
-   def main():
-       rclpy.init()
-       node = PointPublisher()     
-       try:
-           rclpy.spin(node)
-       except KeyboardInterrupt:
-           pass  
-       rclpy.shutdown()
+import rclpy
+from rclpy.node import Node
+
+from turtlesim.msg import Pose
+from turtlesim.srv import Spawn
+
+
+class PointPublisher(Node):
+
+    def __init__(self):
+        super().__init__('turtle_tf2_message_broadcaster')
+
+        self.client = self.create_client(Spawn, 'spawn')
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        request = Spawn.Request()
+        request.name = 'turtle3'
+        request.x = float(4)
+        request.y = float(2)
+        request.theta = float(0)
+        self.client.call_async(request)
+
+        self.vel_pub = self.create_publisher(Twist, '/turtle3/cmd_vel', 1)
+        self.sub = self.create_subscription(Pose, '/turtle3/pose', self.handle_turtle_pose, 1)
+        self.pub = self.create_publisher(PointStamped, '/turtle3/turtle_point_stamped', 1)
+
+    def handle_turtle_pose(self, msg):
+        vel_msg = Twist()
+        vel_msg.linear.x = 1.0
+        vel_msg.angular.z = 1.0
+        self.vel_pub.publish(vel_msg)
+
+        ps = PointStamped()
+        ps.header.stamp = self.get_clock().now().to_msg()
+        ps.header.frame_id = 'world'
+        ps.point.x = msg.x
+        ps.point.y = msg.y
+        ps.point.z = 0.0
+        self.pub.publish(ps)
+
+
+def main():
+    rclpy.init()
+    node = PointPublisher()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    rclpy.shutdown()
