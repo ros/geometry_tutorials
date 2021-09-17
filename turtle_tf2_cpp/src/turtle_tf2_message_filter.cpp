f9a61fdf53733b06956840e4341cdcd46b5248e5
kenny_wang <kennywangaigraphx@gmail.com>
2021-09-16 17:11:01 -0700

    Update CMakeLists.txt and turtle_tf2_message_filter.cpp

--- turtle_tf2_cpp/src/turtle_tf2_message_filter.cpp
+++ turtle_tf2_cpp/src/turtle_tf2_message_filter.cpp
@@ -19,7 +19,11 @@
 #include <tf2_ros/message_filter.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/create_timer_ros.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#ifdef TF2_CPP_HEADERS
+  #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#else
+  #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#endif
 #include <message_filters/subscriber.h>
 
 #include <chrono>
