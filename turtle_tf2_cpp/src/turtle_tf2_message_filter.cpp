af4539ea6b548aa382b3c5d78e6bd72770958122
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-07-11 15:17:36 +0200

    Update turtle_tf2_cpp/src/turtle_tf2_message_filter.cpp
    Co-authored-by: Chris Lalancette <clalancette@gmail.com>

--- turtle_tf2_cpp/src/turtle_tf2_message_filter.cpp
+++ turtle_tf2_cpp/src/turtle_tf2_message_filter.cpp
@@ -52,9 +52,7 @@ public:
     tf2_listener_ =
       std::make_shared<tf2_ros::TransformListener>(*tf2_buffer_);
 
-    point_sub_.subscribe(
-      this, "/turtle3/turtle_point_stamped",
-      rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_default)));
+    point_sub_.subscribe(this, "/turtle3/turtle_point_stamped", rclcpp::QoS(10));
     tf2_filter_ = std::make_shared<tf2_ros::MessageFilter<geometry_msgs::msg::PointStamped>>(
       point_sub_, *tf2_buffer_, target_frame_, 100, this->get_node_logging_interface(),
       this->get_node_clock_interface(), buffer_timeout);
